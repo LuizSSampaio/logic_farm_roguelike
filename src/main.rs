6e8ac12b631b7e04af76948ee37b5b27c0a810fc
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-10-18 23:14:41 -0300

    Added character movement

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ fn main() {
                 }),
         )
         .add_systems(Startup, setup)
+        .add_systems(Update, character_movement)
         .run();
 }
 
@@ -30,4 +31,25 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         texture: asset_server.load("character.png"),
         ..default()
     });
+}
+
+fn character_movement(
+    mut characters: Query<(&mut Transform, &Sprite)>,
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    for (mut transform, _) in &mut characters {
+        if input.pressed(KeyCode::W) {
+            transform.translation.y += 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::S) {
+            transform.translation.y -= 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::D) {
+            transform.translation.x += 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::A) {
+            transform.translation.x -= 100.0 * time.delta_seconds();
+        }
+    }
 }
