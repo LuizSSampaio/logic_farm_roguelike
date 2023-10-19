6b04c5de3b587d6affc61b227fcdac5fdcf900d8
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-10-18 22:43:48 -0300

    Added character texture and some window configurations

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,24 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        resolution: (640.0, 480.0).into(),
+                        title: "Logic Farming Roguelike".into(),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
         .add_systems(Startup, setup)
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
     commands.spawn(SpriteBundle {
@@ -15,6 +27,7 @@ fn setup(mut commands: Commands) {
             custom_size: Some(Vec2::new(100.0, 100.0)),
             ..default()
         },
+        texture: asset_server.load("character.png"),
         ..default()
     });
 }
