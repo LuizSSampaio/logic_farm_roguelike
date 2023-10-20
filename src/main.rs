e29c4747cdab96ee037e92bfac7a7db8b390fbc9
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-10-19 21:58:13 -0300

    Added Buy and Sell pig functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::ScalingMode};
+use pig::PigPlugin;
+
+#[derive(Component)]
+pub struct Player {
+    pub speed: f32,
+}
+
+#[derive(Resource)]
+pub struct Money(pub f32);
+
+mod pig;
 
 fn main() {
     App::new()
@@ -15,37 +26,43 @@ fn main() {
                     ..default()
                 }),
         )
+        .insert_resource(Money(100.0))
+        .add_plugins(PigPlugin)
         .add_systems(Startup, setup)
         .add_systems(Update, character_movement)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
+    let mut camera = Camera2dBundle::default();
 
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(100.0, 100.0)),
+    camera.projection.scaling_mode = ScalingMode::AutoMin {
+        min_width: 256.0,
+        min_height: 144.0,
+    };
+
+    commands.spawn(camera);
+
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("character.png"),
             ..default()
         },
-        texture: asset_server.load("character.png"),
-        ..default()
-    });
+        Player { speed: 100.0 },
+    ));
 }
 
 fn character_movement(
-    mut characters: Query<(&mut Transform, &Sprite)>,
+    mut characters: Query<(&mut Transform, &Player)>,
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
-    let speed = 100.0;
-
-    for (mut transform, _) in &mut characters {
+    for (mut transform, player) in &mut characters {
         let mut dir = Vec2::ZERO;
         dir.x = ((input.pressed(KeyCode::D) as i8) - (input.pressed(KeyCode::A) as i8)) as f32;
         dir.y = ((input.pressed(KeyCode::W) as i8) - (input.pressed(KeyCode::S) as i8)) as f32;
 
         let z = transform.translation.z;
-        transform.translation += Vec3::new(dir.x, dir.y, z) * speed * time.delta_seconds();
+        transform.translation += Vec3::new(dir.x, dir.y, z) * player.speed * time.delta_seconds();
     }
 }
